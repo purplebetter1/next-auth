623a4e033fb7120e24f22dae907d1b5a0e608c26
andrewgrablewski <agrablewski@gmail.com>
2021-12-04 12:50:13 -0500

    change password path

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,32 +1,41 @@
-// import { useState, useEffect } from 'react'
-// import { getSession } from 'next-auth/react'
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  
-  // const [isLoading, setIsLoading] = useState(true)
-  
+  // const [isLoading, setIsLoading] = useState(true);
+
   // useEffect(() => {
-  //   getSession().then(session => {
-  //     if(!session) {
-  //       window.location.href = '/auth'
-  //     }  else {
-  //       setIsLoading(false)
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
   //     }
-  //   })
-  // }, [])
+  //   });
+  // }, []);
 
-  // const { data: session, status: loading } = useSession()
-
-  // if(isLoading) {
-  //   return <p className={classes.profile}>Loading...</p>
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
   // }
+
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
 
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
